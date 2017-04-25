ee70b589e57d1f55188479f55b41cd787156c266
iuri aranda <iuri.aranda@gmail.com>
2017-04-25 10:13:32 +0200

    Added cluster_id output

--- ecs-cluster/outputs.tf
+++ ecs-cluster/outputs.tf
@@ -2,6 +2,10 @@ output "cluster_name" {
   value = "${aws_ecs_cluster.cluster.name}"
 }
 
+output "cluster_id" {
+  value = "${aws_ecs_cluster.cluster.id}"
+}
+
 output "ecs-instance-profile" {
   value = "${aws_iam_instance_profile.ecs-instance-profile.name}"
 }
