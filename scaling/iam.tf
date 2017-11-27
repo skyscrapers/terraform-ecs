339a8a57f8957878b9c0a91d65cc17fb0dc6a765
Luca Venturelli <luca@skyscrapers.eu>
2017-11-27 12:12:22 +0100

    fixing deprecated

--- scaling/iam.tf
+++ scaling/iam.tf
@@ -1,5 +1,6 @@
 resource "aws_iam_role" "ecs-autoscale-role" {
   name = "ecs-scale-${terraform.env}-${var.cluster_name}-${var.service_name}"
+
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -17,11 +18,11 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_autoscale" {
-  role = "${aws_iam_role.ecs-autoscale-role.id}"
+  role       = "${aws_iam_role.ecs-autoscale-role.id}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceAutoscaleRole"
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_cloudwatch" {
-  role = "${aws_iam_role.ecs-autoscale-role.id}"
+  role       = "${aws_iam_role.ecs-autoscale-role.id}"
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
 }
--- scaling/main.tf
+++ scaling/main.tf
@@ -7,10 +7,12 @@ resource "aws_cloudwatch_metric_alarm" "ecs_service_scale_up_alarm" {
   period              = "${var.period_down}"
   statistic           = "${var.statistic}"
   threshold           = "${var.threshold_up}"
+
   dimensions {
     ClusterName = "${var.cluster_name}"
     ServiceName = "${var.service_name}"
   }
+
   alarm_description = "This metric monitor ecs CPU utilization up"
   alarm_actions     = ["${aws_appautoscaling_policy.scale_up.arn}"]
 }
@@ -24,10 +26,12 @@ resource "aws_cloudwatch_metric_alarm" "ecs_service_scale_down_alarm" {
   period              = "${var.period_down}"
   statistic           = "${var.statistic}"
   threshold           = "${var.threshold_down}"
+
   dimensions {
     ClusterName = "${var.cluster_name}"
     ServiceName = "${var.service_name}"
   }
+
   alarm_description = "This metric monitor ecs CPU utilization down"
   alarm_actions     = ["${aws_appautoscaling_policy.scale_down.arn}"]
 }
@@ -42,34 +46,40 @@ resource "aws_appautoscaling_target" "ecs_target" {
 }
 
 resource "aws_appautoscaling_policy" "scale_down" {
-  adjustment_type         = "ChangeInCapacity"
-  cooldown                = 60
-  metric_aggregation_type = "Maximum"
-  name                    = "${terraform.env}-${var.cluster_name}-${var.service_name}-scale-down"
-  resource_id             = "service/${var.cluster_name}/${var.service_name}"
-  scalable_dimension      = "ecs:service:DesiredCount"
-  service_namespace       = "ecs"
+  name               = "${terraform.env}-${var.cluster_name}-${var.service_name}-scale-down"
+  resource_id        = "service/${var.cluster_name}/${var.service_name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
 
-  step_adjustment {
-    metric_interval_upper_bound = "${var.upperbound}"
-    scaling_adjustment          = "${var.scale_down_adjustment}"
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 60
+    metric_aggregation_type = "Maximum"
+
+    step_adjustment {
+      metric_interval_upper_bound = "${var.upperbound}"
+      scaling_adjustment          = "${var.scale_down_adjustment}"
+    }
   }
 
   depends_on = ["aws_appautoscaling_target.ecs_target"]
 }
 
 resource "aws_appautoscaling_policy" "scale_up" {
-  adjustment_type         = "ChangeInCapacity"
-  cooldown                = 60
-  metric_aggregation_type = "Maximum"
-  name                    = "${terraform.env}-${var.cluster_name}-${var.service_name}-scale-up"
-  resource_id             = "service/${var.cluster_name}/${var.service_name}"
-  scalable_dimension      = "ecs:service:DesiredCount"
-  service_namespace       = "ecs"
+  name               = "${terraform.env}-${var.cluster_name}-${var.service_name}-scale-up"
+  resource_id        = "service/${var.cluster_name}/${var.service_name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
 
-  step_adjustment {
-    metric_interval_lower_bound = "${var.lowerbound}"
-    scaling_adjustment          = "${var.scale_up_adjustment}"
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 60
+    metric_aggregation_type = "Maximum"
+
+    step_adjustment {
+      metric_interval_lower_bound = "${var.lowerbound}"
+      scaling_adjustment          = "${var.scale_up_adjustment}"
+    }
   }
 
   depends_on = ["aws_appautoscaling_target.ecs_target"]
