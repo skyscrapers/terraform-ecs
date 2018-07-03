85415d6006e7e6a8448ddc2b039613636bf4468b
Luca Venturelli <luca@skyscrapers.eu>
2018-07-03 11:59:56 +0200

    adding instance profile arn as output (#13)
    * adding instance profile arn as output
    
    * adding readme

--- ecs-cluster/outputs.tf
+++ ecs-cluster/outputs.tf
@@ -10,6 +10,10 @@ output "ecs-instance-profile" {
   value = "${aws_iam_instance_profile.ecs-instance-profile.name}"
 }
 
+output "ecs_instance_profile_arn" {
+  value = "${aws_iam_instance_profile.ecs-instance-profile.arn}"
+}
+
 output "ecs-service-role" {
   value = "${aws_iam_role.ecs-service-role.name}"
 }
